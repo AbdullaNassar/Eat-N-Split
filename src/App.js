b92827762dcd06b19f1d51bd98e2f80cb336bf29
abdo <abdallah moemen44@gmail.com>
2023-09-07 05:22:25 -0700

    Building Static Structure

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import FriendsList from "./component/FriendsList"
+function App() {
+  return (
+    <div className="container">
+      <FriendsList/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/FriendsList.js
@@ -0,0 +1,11 @@
+import Friend from "./Friend";
+
+function FriendsList(){
+    return(
+        <div className='friends-list'>
+            <Friend/>
+        </div>
+    );
+}
+
+export default FriendsList;
